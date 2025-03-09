4d1091e3048d9ee67ce913d6096e66a6ce9b3c5f
Arnaldur Bjarnason <a.arnaldur+git@gmail.com>
2025-03-09 21:49:26 +0100

    added prettier + config & formatted

--- frontend/app/src/main.tsx
+++ frontend/app/src/main.tsx
@@ -1,23 +1,23 @@
-import ReactDOM from 'react-dom/client'
-import { RouterProvider, createRouter } from '@tanstack/react-router'
-import { routeTree } from './routeTree.gen'
+import ReactDOM from "react-dom/client";
+import { RouterProvider, createRouter } from "@tanstack/react-router";
+import { routeTree } from "./routeTree.gen";
 
 // Set up a Router instance
 const router = createRouter({
-  routeTree,
-  defaultPreload: 'intent',
-})
+	routeTree,
+	defaultPreload: "intent",
+});
 
 // Register things for typesafety
-declare module '@tanstack/react-router' {
-  interface Register {
-    router: typeof router
-  }
+declare module "@tanstack/react-router" {
+	interface Register {
+		router: typeof router;
+	}
 }
 
-const rootElement = document.getElementById('app')!
+const rootElement = document.getElementById("app")!;
 
 if (!rootElement.innerHTML) {
-  const root = ReactDOM.createRoot(rootElement)
-  root.render(<RouterProvider router={router} />)
+	const root = ReactDOM.createRoot(rootElement);
+	root.render(<RouterProvider router={router} />);
 }
--- frontend/app/src/routes/__root.tsx
+++ frontend/app/src/routes/__root.tsx
@@ -1,34 +1,37 @@
-import { Link, Outlet, createRootRoute } from '@tanstack/react-router'
-import { TanStackRouterDevtools } from '@tanstack/router-devtools'
-import { AppBar, Toolbar, Typography, Button, Container, Box } from '@mui/material'
+import { Link, Outlet, createRootRoute } from "@tanstack/react-router";
+import { TanStackRouterDevtools } from "@tanstack/router-devtools";
+import {
+	AppBar,
+	Toolbar,
+	Typography,
+	Button,
+	Container,
+	Box,
+} from "@mui/material";
 
 export const Route = createRootRoute({
-  component: RootComponent,
-})
+	component: RootComponent,
+});
 
 function RootComponent() {
-  return (
-    <>
-      <AppBar position="static">
-        <Toolbar>
-          <Typography variant="h6" sx={{ flexGrow: 1 }}>
-            Neurons Demo
-          </Typography>
-          <Button
-            color="inherit" 
-            component={Link}
-            to="/dashboard"
-          >
-            Dashboard
-          </Button>
-        </Toolbar>
-      </AppBar>
-      <Container>
-        <Box sx={{ mt: 4 }}>
-          <Outlet />
-        </Box>
-      </Container>
-      <TanStackRouterDevtools position="bottom-right" />
-    </>
-  )
+	return (
+		<>
+			<AppBar position="static">
+				<Toolbar>
+					<Typography variant="h6" sx={{ flexGrow: 1 }}>
+						Neurons Demo
+					</Typography>
+					<Button color="inherit" component={Link} to="/dashboard">
+						Dashboard
+					</Button>
+				</Toolbar>
+			</AppBar>
+			<Container>
+				<Box sx={{ mt: 4 }}>
+					<Outlet />
+				</Box>
+			</Container>
+			<TanStackRouterDevtools position="bottom-right" />
+		</>
+	);
 }
