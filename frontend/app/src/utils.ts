223bec2eb8fc6d97a2c2b58c012f48c2ce547cc1
Arnaldur Bjarnason <a.arnaldur+git@gmail.com>
2025-03-10 20:41:58 +0100

    added env configuration to frontend

--- frontend/app/src/utils.ts
+++ frontend/app/src/utils.ts
@@ -1,4 +1,3 @@
-// Use environment variable if available, otherwise fallback to localhost for development
 export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export async function easyPost(
