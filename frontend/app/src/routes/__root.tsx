41fbf230f05bc145fd47d135fe78bb6370ab177c
Arnaldur Bjarnason <a.arnaldur+git@gmail.com>
2025-03-10 11:01:04 +0100

    added hamburger menu for small screens + trading

--- frontend/app/src/routes/__root.tsx
+++ frontend/app/src/routes/__root.tsx
@@ -7,7 +7,14 @@ import {
 	Button,
 	Container,
 	Box,
+	useMediaQuery,
+	IconButton,
+	Menu,
+	MenuItem,
+	useTheme,
 } from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+import { useState } from "react";
 
 import { NotificationProvider } from "./-Notifications";
 
@@ -16,6 +23,10 @@ export const Route = createRootRoute({
 });
 
 function RootComponent() {
+	const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+	const theme = useTheme();
+	const isSmallScreen = useMediaQuery(theme.breakpoints.down("sm"));
+
 	return (
 		<>
 			<AppBar position="static">
@@ -23,16 +34,37 @@ function RootComponent() {
 					<Typography variant="h6" sx={{ flexGrow: 1 }}>
 						Neurons Demo
 					</Typography>
-					<Button color="inherit" component={Link} to="/dashboard">
-						Dashboard
-					</Button>
-					<Button
-						color="inherit"
-						component={Link}
-						to="/register-survivor"
-					>
-						Register Survivor
-					</Button>
+
+					{isSmallScreen ? (
+						<HamburgerMenu
+							anchorEl={anchorEl}
+							setAnchorEl={setAnchorEl}
+						/>
+					) : (
+						<>
+							<Button
+								color="inherit"
+								component={Link}
+								to="/dashboard"
+							>
+								Dashboard
+							</Button>
+							<Button
+								color="inherit"
+								component={Link}
+								to="/trading"
+							>
+								Trading
+							</Button>
+							<Button
+								color="inherit"
+								component={Link}
+								to="/register-survivor"
+							>
+								Register Survivor
+							</Button>
+						</>
+					)}
 				</Toolbar>
 			</AppBar>
 			<Container>
@@ -43,6 +75,63 @@ function RootComponent() {
 				</Box>
 			</Container>
 			<TanStackRouterDevtools position="bottom-right" />
+		</>
+	);
+}
+function HamburgerMenu({
+	anchorEl,
+	setAnchorEl,
+}: {
+	anchorEl: HTMLElement | null;
+	setAnchorEl: (e: HTMLElement | null) => void;
+}) {
+	const handleMenu = (event: React.MouseEvent<HTMLElement>) => {
+		setAnchorEl(event.currentTarget);
+	};
+
+	const handleClose = () => {
+		setAnchorEl(null);
+	};
+	return (
+		<>
+			<IconButton
+				size="large"
+				edge="end"
+				color="inherit"
+				aria-label="menu"
+				onClick={handleMenu}
+			>
+				<MenuIcon />
+			</IconButton>
+			<Menu
+				id="menu-appbar"
+				anchorEl={anchorEl}
+				anchorOrigin={{
+					vertical: "top",
+					horizontal: "right",
+				}}
+				keepMounted
+				open={Boolean(anchorEl)}
+				onClose={handleClose}
+			>
+				<MenuItem
+					component={Link}
+					to="/dashboard"
+					onClick={handleClose}
+				>
+					Dashboard
+				</MenuItem>
+				<MenuItem component={Link} to="/trading" onClick={handleClose}>
+					Trading
+				</MenuItem>
+				<MenuItem
+					component={Link}
+					to="/register-survivor"
+					onClick={handleClose}
+				>
+					Register Survivor
+				</MenuItem>
+			</Menu>
 		</>
 	);
 }
